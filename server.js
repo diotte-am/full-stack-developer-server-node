0cbd82b7be8fc1918a1e26653ecb1202d0d935c3
diotte-am <83765247+diotte-am@users.noreply.github.com>
2022-04-03 10:11:12 -0400

    RESTful Web service APIs

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import express from 'express';
 import helloController from "./controllers/hello-controller.js";
 import welcomeController from "./controllers/welcome-controller.js";
 import userController from "./controllers/user-controller.js";
+import tuitsController from "./controllers/tuits-controller.js";
 
 const app = express();
 helloController(app);
 welcomeController(app);
 app.use(express.json());
 userController(app);
+tuitsController(app);
 app.listen(4000);
