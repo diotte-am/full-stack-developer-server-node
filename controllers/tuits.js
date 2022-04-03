71a79c31fc9286ff80dd9e70b6f35c9ba83a0f3e
diotte-am <83765247+diotte-am@users.noreply.github.com>
2022-04-03 19:28:40 -0400

    a8

--- controllers/tuits.js
+++ controllers/tuits.js
@@ -1,133 +1,59 @@
 export default [
     {
-        "_id": "123",
-        "topic": "Web Development",
-        "postedBy": {
-            "username": "ReactJS"
+        _id: "123",
+        topic: "Web Development",
+        postedBy: {
+            username: "ReactJS"
         },
-        "liked": true,
-        "disliked": false,
-        "verified": false,
-        "handle": "ReactJS",
-        "time": "2h",
-        "title": "React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs",
-        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        "attachments": {
-            "video": "unKvMC3Y1kI"
+        liked: true,
+        disliked: false,
+        verified: false,
+        handle: "ReactJS",
+        time: "2h",
+        title: "React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs",
+        tuit: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        attachments: {
+            video: "unKvMC3Y1kI"
         },
-        "logoImage": "/tuiter/image/react-logo.png",
-        "avatarImage": "/tuiter/image/react-logo.png",
-        "likes": 365,
-        "dislikes": 355,
-        "stats": {
-            "comments": 123,
-            "retuits": 234
+        logoImage: "/tuiter/image/react-logo.png",
+        avatarImage: "/tuiter/image/react-logo.png",
+        likes: 365,
+        dislikes: 355,
+        stats: {
+            comments: 123,
+            retuits: 234
 
         }
     },
 
     {
-        "_id": "567",
-        "topic": "Space",
-        "postedBy": {
-            "username": "SpaceX"
+        _id: "567",
+        topic: "Space",
+        postedBy: {
+            username: "SpaceX"
         },
-        "liked": false,
-        "disliked": true,
-        "verified": false,
-        "handle": "spacex",
-        "title": "",
-        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        "attachments": {
-            "image": "/tuiter/image/starship.jpg"
+        liked: false,
+        disliked: true,
+        verified: false,
+        handle: "spacex",
+        title: "",
+        tuit: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        attachments: {
+            imag": "/tuiter/image/starship.jpg"
         },
-        "time": "last month",
-        "logoImage": "/tuiter/image/nodejs.png",
-        "avatarImage": "/tuiter/image/nodejs.png",
-        "tuits": "120K",
-        "stats": {
-            "comments": 123,
-            "retuits": 234
+        time: "last month",
+        logoImage: "/tuiter/image/nodejs.png",
+        avatarImage: "/tuiter/image/nodejs.png",
+        tuits: "120K",
+        stats: {
+            comments: 123,
+            retuits: 234
 
         },
-        "likes": 345,
-        "dislikes": 355
-    },
-
-    {
-        "_id": "234",
-        "topic": "Space",
-        "postedBy": {
-            "username": "Scott Manley"
-        },
-        "liked": true,
-        "disliked": false,
-        "verified": true,
-        "handle": "DJSnM",
-        "postedOn": "2020-12-10",
-        "time": "1 day",
-        "title": "JavaScript is programming language that can run on browsers as well as desktops",
-        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        "logoImage": "/tuiter/image/javascript.png",
-        "avatarImage": "/tuiter/image/javascript.png",
-        "tuits": "123K",
-        "likes": 345,
-        "dislikes": 355,
-        "stats": {
-            "comments": 123,
-            "retuits": 234
-
-        }
-    },
-
-    {
-        "_id": "345",
-        "topic": "Web Development",
-        "postedBy": {
-            "username": "jQuery"
-        },
-        "liked": false,
-        "disliked": false,
-        "verified": false,
-        "handle": "jQuery",
-        "title": "jQuery",
-        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        "time": "last week",
-        "logoImage": "/tuiter/image/jquery.png",
-        "avatarImage": "/tuiter/image/jquery.png",
-        "tuits": "122K",
-        "likes": 345,
-        "dislikes": 355,
-        "stats": {
-            "comments": 123,
-            "retuits": 234
-
-        }
-    },
-
-    {
-        "_id": "456",
-        "topic": "Web Development",
-        "postedBy": {
-            "username": "NodeJS"
-        },
-        "liked": true,
-        "disliked": false,
-        "verified": true,
-        "handle": "NodeJS",
-        "title": "",
-        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        "time": "last month",
-        "logoImage": "/tuiter/image/nodejs.png",
-        "avatarImage": "/tuiter/image/nodejs.png",
-        "tuits": "120K",
-        "likes": 345,
-        "dislikes": 355,
-        "stats": {
-            "comments": 123,
-            "retuits": 234
-
-        }
+        likes: 345,
+        dislikes: 355
     }
+
+
 
 ];
