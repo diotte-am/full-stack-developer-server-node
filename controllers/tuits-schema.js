88e523f83877341142c7c471535bd7ca47b8b096
diotte-am <83765247+diotte-am@users.noreply.github.com>
2022-04-15 16:43:27 -0400

    a9 update schema

--- controllers/tuits-schema.js
+++ controllers/tuits-schema.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
     tuit: String,
+    liked: Boolean,
+    disliked: Boolean,
     likes: Number,
     dislikes: Number,
     postedBy: {
